81ae84df9eace61fd99afbd70aa3d19ae646419d
avintimilsina <avin.timilsina.16@gmail.com>
2023-09-04 20:15:12 +0545

    fixed deprecated packages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -186,8 +186,8 @@
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer__about">
                         <div class="footer__about__logo">
-                            <a href="./index.html"><img src="img/logo.png" alt=""></a>
-                        </div>
+                           .html"><img src="img/logo.png" alt=""></a>
+                        </div> <a href="./index
                         <ul>
                             <li>Address: 60-49 Road 11378 New York</li>
                             <li>Phone: +65 11.188.888</li>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,6 +1,5 @@
 @extends('layout')
 @section('content')
-    <!-- Hero Section Begin -->
     <section class="hero hero-normal">
         <div class="container">
             <div class="row">
